768265e7d80d9b885f504c5c9393ae6209d5a14e
Danilo <eudanilobarbosa@users.noreply.github.com>
2024-05-05 19:30:53 -0300

    Seção 3: Automação Web com Cucumber  > 21. Validando o comportamento esperado

--- features/step_definitions/pedido_steps.rb
+++ features/step_definitions/pedido_steps.rb
@@ -19,12 +19,9 @@ Quando('por fim finalizo a compra') do
 end
 
 Então('sou redirecionado para a página de confirmação de Pedidos') do
-pending # Write code here that turns the phrase above into concrete actions
+    @order.assert_order
 end
 
-Então('deve ser informado um prazo de entrega entre {int} a {int} minutos') do |int, int2|
-# Então('deve ser informado um prazo de entrega entre {int} a {float} minutos') do |int, float|
-# Então('deve ser informado um prazo de entrega entre {float} a {int} minutos') do |float, int|
-# Então('deve ser informado um prazo de entrega entre {float} a {float} minutos') do |float, float2|
-pending # Write code here that turns the phrase above into concrete actions
+Então('deve ser informado o seguinte prazo de entrega: {string}') do |delivery_time|
+    @order.assert_delivery_time(delivery_time)
 end
--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,11 +1,12 @@
 
 require_relative './pages/home.rb'
 require_relative './pages/checkout.rb'
+require_relative './pages/order.rb'
 require_relative './pages/components.rb'
 
 Before do
     @home = HomePage.new
     @checkout = CheckoutPage.new
-
+    @order = OrderPage.new
     @popup = Popup.new
 end
